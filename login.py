f3596df3822795249ca7c45f164ae2ec4660b2e0
Daniel Petti <djpetti@gmail.com>
2014-05-02 23:09:35 -0700

    Got some simple stuff working.
    - Login and logout links appear, and prompt user for google
    accounts authentication.
    
    - For some reason, autogenerated files were being tracked, so I
    made a real .gitignore file.
    
    - Changed some style things that were bothering me.

---
+++ login.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import webapp2
+
+from google.appengine.api import users
+
+class LoginHandler(webapp2.RequestHandler):
+  def get(self):
+    user = users.get_current_user()
+    if not user:
+      # Logging in.
+      self.redirect(users.create_login_url("/"))
+    else:
+      # Logging out.
+      self.redirect(users.create_logout_url("/"))
+
+application = webapp2.WSGIApplication([
+    ("/login", LoginHandler),
+    ], debug = True)
