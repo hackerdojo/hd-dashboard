5402c128e4f4aa59f40cb925eefce143ec1a8536
Daniel Petti <djpetti@gmail.com>
2014-05-03 09:34:26 -0700

    Cleaned up code and added greeting.

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 import webapp2
 from google.appengine.api import users
 from google.appengine.ext.webapp import template
-#from google.appengine.ext import webapi
 
 class MainHandler(webapp2.RequestHandler):
   def get(self):
@@ -11,8 +10,10 @@ class MainHandler(webapp2.RequestHandler):
     values = {}
     if not user:
       values["login_text"] = "Login"
+      values["greeting"] = ""
     else:
       values["login_text"] = "Logout"
+      values["greeting"] = "Hello, %s!" % (user.nickname())
 
     self.response.out.write(template.render("main.html", values))
 
