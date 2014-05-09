0324dc31c3ff2fabec6c287e14dab4def6f156c7
Daniel Petti <djpetti@gmail.com>
2014-05-08 19:59:18 -0700

    Make modifications for dev version of dashboard.
    It now sends you to the dev version of the signup app.
    As we add dev versions of other apps, we will have it send you
    to those as well.

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ from google.appengine.ext.webapp import template
 
 class MainHandler(webapp2.RequestHandler):
   def get(self):
+    # Check if we're in the dev version.
+    url = self.request.url
+    dev = False
+    if "-dev" in url:
+      dev = True
+
     user = users.get_current_user()
     values = {}
     if not user:
@@ -14,6 +20,12 @@ class MainHandler(webapp2.RequestHandler):
     else:
       values["login_text"] = "Logout"
       values["greeting"] = "Hello, %s!" % (user.nickname())
+    # If we're in the dev version, it should send people to the dev version of
+    # other apps.
+    if dev:
+      values["signup_url"] = "http://signup-dev.hackerdojo.com/key"
+    else:
+      values["signup_url"] = "http://signup.hackerdojo.com/key"
 
     self.response.out.write(template.render("main.html", values))
 
