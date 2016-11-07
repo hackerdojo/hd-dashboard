444bb5dfb818a8680a89ca17b149f8875d95639f
Celine Bursztein <bitbucket@celine.im>
2016-11-07 15:42:39 -0800

    CSS and viewport debug for dashboard. Locker icon added to dashboard. Locker rental page and confirmation page added. Favicon added.

--- main.py
+++ main.py
@@ -1,36 +1,89 @@
 #!/usr/bin/env python
+import logging
 
 import webapp2
 from google.appengine.api import users
 from google.appengine.ext.webapp import template
 
+
 class MainHandler(webapp2.RequestHandler):
-  def get(self):
-    # Check if we're in the dev version.
-    url = self.request.url
-    dev = False
-    if "-dev" in url:
-      dev = True
+    def get(self):
+        # Check if we're in the dev version.
+        url = self.request.url
+        dev = False
+        if "-dev" in url:
+            dev = True
 
-    user = users.get_current_user()
-    values = {}
-    if not user:
-      values["isLogin"] = True
-      values["login_text"] = "Login"
-      values["greeting"] = ""
-    else:
-      values["isLogin"] = False
-      values["login_text"] = "Logout %s" % (user.nickname())
-    # If we're in the dev version, it should send people to the dev version of
-    # other apps.
-    if dev:
-      values["dev_message"] = "You are using the dev version of Dashboard."
-      values["signup_url"] = "http://signup-dev.appspot.com"
-    else:
-      values["signup_url"] = "http://signup.hackerdojo.com"
+        user = users.get_current_user()
+        values = {}
+        if not user:
+            values["isLogin"] = False
+            values["login_text"] = "Login"
+            values["greeting"] = ""
+        else:
+            values["isLogin"] = True
+            values["login_text"] = "Logout %s" % (user.nickname())
+        # If we're in the dev version, it should send people to the dev version of
+        # other apps.
+        if dev:
+            values["dev_message"] = "You are using the dev version of Dashboard."
+            values["signup_url"] = "http://signup-dev.appspot.com"
+        else:
+            values["signup_url"] = "http://signup.hackerdojo.com"
 
-    self.response.out.write(template.render("main.html", values))
+        self.response.out.write(template.render("main.html", values))
+
+
+class LockerHandler(webapp2.RequestHandler):
+    def get(self):
+        # Check if we're in the dev version.
+        url = self.request.url
+        dev = False
+        if "-dev" in url:
+            dev = True
+
+        user = users.get_current_user()
+        values = {}
+        if not user:
+            values["isLogin"] = False
+            values["login_text"] = "Login"
+            values["greeting"] = ""
+            values['member_email'] = ""
+            self.redirect("/login")
+            return
+        else:
+            values['member_email'] = user.email()
+            values["isLogin"] = True
+            values["login_text"] = "Logout %s" % (user.nickname())
+
+        self.response.out.write(template.render("locker.html", values))
+
+
+class LockerConfirmationHandler(webapp2.RequestHandler):
+    def get(self):
+        # Check if we're in the dev version.
+        url = self.request.url
+        dev = False
+        if "-dev" in url:
+            dev = True
+
+        user = users.get_current_user()
+        values = {}
+        if not user:
+            values["isLogin"] = False
+            values["login_text"] = "Login"
+            values["greeting"] = ""
+            values['member_email'] = ""
+        else:
+            values['member_email'] = user.email()
+            values["isLogin"] = True
+            values["login_text"] = "Logout %s" % (user.nickname())
+
+        self.response.out.write(template.render("lockerconfirmation.html", values))
+
 
 application = webapp2.WSGIApplication([
     ("/", MainHandler),
-    ], debug=True)  
+    ("/locker", LockerHandler),
+    ("/lockerconfirmation", LockerConfirmationHandler)
+], debug=True)
