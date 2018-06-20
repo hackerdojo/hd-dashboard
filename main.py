8e92e4990266b123406dbf4e657c918cf1042809
Celine Bursztein <contact@celine.im>
2018-06-20 13:56:58 -0700

    Mailbox rental in dashboard

--- main.py
+++ main.py
@@ -82,8 +82,58 @@ class LockerConfirmationHandler(webapp2.RequestHandler):
         self.response.out.write(template.render("lockerconfirmation.html", values))
 
 
+class MailboxHandler(webapp2.RequestHandler):
+    def get(self):
+        # Check if we're in the dev version.
+        url = self.request.url
+        dev = False
+        if "-dev" in url:
+            dev = True
+
+        user = users.get_current_user()
+        values = {}
+        if not user:
+            values["isLogin"] = False
+            values["login_text"] = "Login"
+            values["greeting"] = ""
+            values['member_email'] = ""
+            self.redirect("/login")
+            return
+        else:
+            values['member_email'] = user.email()
+            values["isLogin"] = True
+            values["login_text"] = "Logout %s" % (user.nickname())
+
+        self.response.out.write(template.render("mailbox.html", values))
+
+
+class MailboxConfirmationHandler(webapp2.RequestHandler):
+    def get(self):
+        # Check if we're in the dev version.
+        url = self.request.url
+        dev = False
+        if "-dev" in url:
+            dev = True
+
+        user = users.get_current_user()
+        values = {}
+        if not user:
+            values["isLogin"] = False
+            values["login_text"] = "Login"
+            values["greeting"] = ""
+            values['member_email'] = ""
+        else:
+            values['member_email'] = user.email()
+            values["isLogin"] = True
+            values["login_text"] = "Logout %s" % (user.nickname())
+
+        self.response.out.write(template.render("mailboxconfirmation.html", values))
+
+
 application = webapp2.WSGIApplication([
     ("/", MainHandler),
     ("/locker", LockerHandler),
-    ("/lockerconfirmation", LockerConfirmationHandler)
+    ("/lockerconfirmation", LockerConfirmationHandler),
+    ("/mailbox", MailboxHandler),
+    ("/mailboxconfirmation", MailboxConfirmationHandler)
 ], debug=True)
